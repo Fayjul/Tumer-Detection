6fa847c8b9da50cddd7078440fb1cf0483ef1689
Fayjul Islam <fayzulislam9@gmail.com>
2022-10-28 21:26:39 +0600

    Page design added Alhamdulillah

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, useNavigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-div">
+        <form onSubmit={(event) => console.log('hello')}>
+          <div className="brand">
+            <img alt=""></img>
+            <h1>Tumer Detection</h1>
+          </div>
+
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            // onChange={(e) => handleChange(e)}
+          />
+
+          <button type="submit"> Submit </button>
+        </form>
+      </div>
+    </>
   );
 }
 
